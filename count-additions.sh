2b8c2134e44bebe949d74c83a81dbe09ce6a7a2f
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-03-25 00:36:22 +0200

    Updated the counting script

--- count-additions.sh
+++ count-additions.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-SUBMOD="ui-star-names"
+CELESTIAL_BODIES_DIR="ui-star-names/common/random_names/base"
 
-CELESTIAL_BODIES_DIR="common/random_names/base"
-CELESTIAL_BODIES_COUNT=$(cat $SUBMOD/$CELESTIAL_BODIES_DIR/* | sed 's/ //g' | sed '/^\s*$\|=\|}\|^#/d' | uniq | wc -l)
+ASTEROID_POSTFIXES_COUNT=$(cat $CELESTIAL_BODIES_DIR/ui_asteroid-postfixes* | sed 's/ //g' | sed '/^\s*$\|=\|}\|^#/d' | sort | sed 's/\"//g' | uniq | wc -l)
+ASTEROID_PREFIXES_COUNT=$(cat $CELESTIAL_BODIES_DIR/ui_asteroid-prefixes* | sed 's/ //g' | sed '/^\s*$\|=\|}\|^#/d' | sort | sed 's/\"//g' | uniq | wc -l)
+BLACK_HOLES_COUNT=$(cat $CELESTIAL_BODIES_DIR/ui_black-holes* | sed 's/ //g' | sed '/^\s*$\|=\|}\|^#/d' | sort | sed 's/\"//g' | uniq | wc -l)
+NEBULAS_COUNT=$(cat $CELESTIAL_BODIES_DIR/ui_nebulas* | sed 's/ //g' | sed '/^\s*$\|=\|}\|^#/d' | sort | sed 's/\"//g' | uniq | wc -l)
+STARS_COUNT=$(cat $CELESTIAL_BODIES_DIR/ui_stars* | sed 's/ //g' | sed '/^\s*$\|=\|}\|^#/d' | sort | sed 's/\"//g' | uniq | wc -l)
 
-echo "Celestial bodies:" $CELESTIAL_BODIES_COUNT
-
+echo "Asteroid Postfixes:" $ASTEROID_POSTFIXES_COUNT
+echo "Asteroid Prefixes:" $ASTEROID_PREFIXES_COUNT
+echo "Black Holes:" $BLACK_HOLES_COUNT
+echo "Nebulas:" $NEBULAS_COUNT
+echo "Stars:" $STARS_COUNT
